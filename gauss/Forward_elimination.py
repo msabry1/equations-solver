451dcc1f5675d29c9e587300e4de3c5ba0e1b8d9
alimk754 <149207595+alimk754@users.noreply.github.com>
2024-11-25 19:18:29 +0200

    Add files via upload

---
+++ gauss/Forward_elimination.py
@@ -0,0 +1,69 @@
+import copy
+import numpy as np
+class forward_eliminator:
+   
+    def __init__(self,array,scalers):
+        self.array=array
+        self.scalers=scalers
+        self.tol=1e-9
+    def pivoting(self, row):
+        index = row
+        flag=False
+        max_ratio = abs(self.array[row][row] / self.scalers[row])
+        for i in range(row + 1, len(self.array)):
+            current_ratio = abs(self.array[i][row] / self.scalers[i])
+            if current_ratio > max_ratio:
+                max_ratio = current_ratio
+                index = i
+        if index != row:
+            self.array[[row, index]] = self.array[[index, row]]
+            self.scalers[row], self.scalers[index] = self.scalers[index], self.scalers[row]
+            flag=True
+        return self.array,flag
+    def Forward_Elimination(self):
+        arr = self.array
+        factors=[]
+        rows = len(arr)
+        cols = len(arr[0])
+        for k in range(rows):
+            self.pivoting(k)
+            if abs(arr[k][k] / self.scalers[k]) < self.tol:
+                return -1
+            for i in range(k + 1, rows):
+                idx=0
+                factor = arr[i][k] / arr[k][k]
+          
+                for j in range(k+1, cols):
+                    arr[i][j] -= factor * arr[k][j]
+        if abs(arr[rows - 1][rows - 1] / self.scalers[rows - 1]) < self.tol:
+            return -1
+        self.array = arr
+        return factor
+    def Forward_Elimination_nextStep(self):
+
+        flag = True 
+        arr = self.array
+        rows = len(arr)
+        cols = len(arr[0])
+
+        for i in range(rows):
+            arr,bool=self.pivoting(i)
+            if abs(arr[i][i] / self.scalers[i]) < self.tol:
+                    return -1   
+            if bool==True:
+                return False,arr
+            if any(arr[j][i] != 0 for j in range(i + 1, rows)):
+                flag = False
+                 
+                for j in range(i + 1, rows):
+               
+                    if arr[j][i] != 0: 
+                        factor = arr[j][i] / arr[i][i]
+                        for k in range(i, cols):
+                            arr[j][k] =arr[j][k]- (factor * arr[i][k])   
+                        return flag,arr
+        if abs(arr[rows - 1][rows - 1] / self.scalers[rows - 1]) < self.tol:
+            return -1        
+        return flag,arr
+
+
--- gauss/gauss.py
+++ gauss/gauss.py
@@ -1,19 +1,22 @@
 import numpy as np
+from collections import deque
+import copy
+import Forward_elimination
 
 class gauss:
-    array=np.ones((3,3))
-    answer=np.zeros(3)
-    scalers=np.zeros(3)
-    tol=1e-9
     def __init__(self,n):
+        
         self.array=n
-        self.answer=np.zeros(len(self.array))
-        self.scalers=np.zeros(len(self.array))
+        self.answer=np.zeros(len(self.array),dtype=float)
+        self.factors=np.zeros(len(self.array),dtype=float)
+        self.scalers=self.scaling()
+        self.eliminator=Forward_elimination.forward_eliminator(self.array,self.scalers)
+
     
     def scaling(self):
         arr = self.array
         self.scalers = [0]*len(arr)
-
+        
         for i in range(len(arr)): 
             max_val = abs(arr[i][0]) 
 
@@ -23,56 +26,52 @@ class gauss:
         
             self.scalers[i] = max_val  
         return self.scalers   
-    def pivoting(self, row):
-        index = row
-        mx = abs(self.array[row][row] / self.scalers[row]) 
-        for i in range(row + 1, len(self.array)):
-            tmp = abs(self.array[i][row] / self.scalers[i])
-            if tmp > mx:
-                mx = tmp
-                index = i
-        if index != row:
-            self.array[row], self.array[index] = self.array[index], self.array[row]
-            self.scalers[row], self.scalers[index] = self.scalers[index], self.scalers[row]
-        return self.array
-    def Forward_Elimination(self):
-        arr = self.array
-        rows = len(arr)
-        cols = len(arr[0])
-        for k in range(rows):
-            self.pivoting(k)
-            if(abs(arr[k][k])/self.scalers[k]<self.tol):
-                return -1
-
-            for i in range(k + 1, rows):
-                factor = arr[i][k] / arr[k][k]
-                for j in range(k, cols):
-                    arr[i][j] -= factor * arr[k][j]
-            if(abs(arr[rows-1][rows-1]/self.scalers[rows-1])<self.tol):
-                return -1
-        self.array=arr
-
     def Backward_Substitution(self):
-        res=self.answer
+       
         arr=self.array
         col=len(arr[0])
         rows=len(arr)
-        res[rows-1]=arr[rows-1][col-1]/arr[rows-1][rows-1]
+        self.answer[rows-1]=arr[rows-1][col-1]/arr[rows-1][rows-1]
         for i in range(rows-2,-1,-1):
             sum=0
             for j in range (i+1,rows):
-                sum+=arr[i][j]*res[j]
-            res[i]=(arr[i][col-1]-sum)/arr[i][i]
-        return res        
+                sum+=arr[i][j]*self.answer[j]
+            self.answer[i]=(arr[i][col-1]-sum)/arr[i][i]
+         
+        return self.answer        
     def Gauss(self):
-        self.scalers=self.scaling()
-        if(self.Forward_Elimination()!=-1):
+        if(self.eliminator.Forward_Elimination()!=-1):
             self.answer=self.Backward_Substitution()
         else:
             return -1    
-        return self.answer                
-m1=gauss([[1,5,1,4],[1,-1,5,7],[1,-2,5,12]])
-
+        return np.array(self.answer)    
+    def next_Step(self):
+        ind=self.eliminator.Forward_Elimination_nextStep()
+        if ind==-1:
+            return -1
+        flag,arr=ind
+        if flag==True:
+            return self.Backward_Substitution()
+        else:
+            self.array=arr  
+            return arr
+m1=gauss(np.array([[1,2,2,3],[5,5,2,5],[3,4,6,7]],dtype=float))
 print(m1.Gauss())
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
     
 
