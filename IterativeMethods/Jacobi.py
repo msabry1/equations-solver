992493f0969b661d9dbb254fed7a7d861804608d
Abdelrahman <Abdelrahmansaeed@gmail.com>
2024-11-25 14:59:25 +0200

    feat: jacobi and siedel implementation

--- IterativeMethods/Jacobi.py
+++ IterativeMethods/Jacobi.py
@@ -8,7 +8,7 @@ class jacobi (iterations):
     def getResult(self, iteration):
         currentRes = self.Initials 
         for i in range(iteration):
-            finalRes = [
+            finalRes = []
             for j in range(len(self.Initials)):
                 sum_ = sum(self.MatrixA[j][k] * currentRes[k] for k in range(len(self.Initials)) if k != j)
                 finalRes.append((self.MatrixB[j] - sum_) / self.MatrixA[j][j]) #B is matrix of values
